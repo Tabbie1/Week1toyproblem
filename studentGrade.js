2260083738d8f06a67e9e9d75d4d1ae62e1f7d1e
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-17 11:02:21 +0300

    add the first condition

--- studentGrade.js
+++ studentGrade.js
@@ -1,7 +1,8 @@
 // Create a new function named studentGrade that takes one parameter named score
 function studentGrade(score){
+    // add the first condition
     if (score>79){
         return 'A';
     }
-
+  
 }
