a6485c5c9c4a39e923e9a7b209167a4d46e585e2
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-17 11:30:10 +0300

    added curly brackets to primary condition

--- speed.js
+++ speed.js
@@ -1,8 +1,10 @@
 //write a function named speedDetector with a parameter named reading
 function speedDetector(reading){
 // add first condition
-    if (reading<70);
-    return 'OK';
+    if (reading<70){
+        return 'OK';
+    }
+    
 
 
 }
