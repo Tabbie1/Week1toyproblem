69ef5f2fa949654f5e99570e65d7452f1eb53134
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-17 22:46:26 +0300

    introduced ranges for nhif

--- netSalary.js
+++ netSalary.js
@@ -33,6 +33,58 @@ else if(taxablePay>=500001 && taxablePay<=800000){
 else {
     payee=0.35*taxablePay;
 }
+// introduce conditions and ranges for nhif
+if (grossSalary<=5999){
+    nhif=150;
+}
+else if(grossSalary>=6000 && grossSalary<=7999){
+    nhif=300;
+}
+else if(grossSalary>=8000 && grossSalary<=11900){
+    nhif=400;
+}
+else if(grossSalary>=12000 && grossSalary<=14999){
+    nhif=500;
+}
+else if(grossSalary>=15000 && grossSalary<=19999){
+    nhif=600;
+}
+else if(grossSalary>=20000 && grossSalary<=24999){
+    nhif=750;
+}
+else if(grossSalary>=25000 && grossSalary<=29999){
+    nhif=850;
+}
 
+else if(grossSalary>=30000 && grossSalary<=34999){
+    nhif=900;
+}
+else if(grossSalary>=35000 && grossSalary<=39999){
+    nhif=950;
+}
+else if(grossSalary>=40000 && grossSalary<=44999){
+    nhif=1000;
+}
+else if(grossSalary>=45000 && grossSalary<=49999){
+    nhif=1100;
+}
+else if(grossSalary>=50000 && grossSalary<=59999){
+    nhif=1200;
+}
+else if(grossSalary>=60000 && grossSalary<=69999){
+    nhif=1300;
+}
+else if(grossSalary>=70000 && grossSalary<=79999){
+    nhif=1400;
+}
+else if(grossSalary>=80000 && grossSalary<=89999){
+    nhif=1500;
+}
+else if(grossSalary>=90000 && grossSalary<=99999){
+    nhif=1600;
+}
+else {
+    nhif=1700;
+}
 
 }
