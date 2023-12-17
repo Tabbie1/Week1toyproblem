2f9c941f6289d035a384d6708b2a1620d333b071
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-17 22:44:53 +0300

    add conditions for payee

--- netSalary.js
+++ netSalary.js
@@ -15,4 +15,24 @@ function salaryPayment(basicSalary,benefits){
        nssf=1080;
    }
      
+//introduce conditions and ranges for payee
+   if (taxablePay<=24000){
+    payee=0.01*taxablePay;
+}
+else if(taxablePay>=24001 && taxablePay<=32333){
+    payee=0.25*taxablePay;
+}
+else if(taxablePay>=32334 && taxablePay<=500000){
+  
+    payee=0.3*taxablePay;
+  
+}
+else if(taxablePay>=500001 && taxablePay<=800000){
+    payee=0.325*taxablePay;
+}
+else {
+    payee=0.35*taxablePay;
+}
+
+
 }
